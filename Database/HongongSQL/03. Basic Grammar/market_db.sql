65dd07aa2ba41be419472094c59442e2fd60a778
daeyeob kim <kimdyk2@hanmail.net>
2022-05-18 07:12:19 +0900

    예시 sql 데이터 추가

--- Database/HongongSQL/03. Basic Grammar/market_db.sql
+++ Database/HongongSQL/03. Basic Grammar/market_db.sql
@@ -87,3 +87,16 @@ SELECT mem_name, height
     
 -- DISTINCT 예시
 SELECT DISTINCT addr FROM member;   -- DISTINCT를 열 이름 앞에 붙이면 중복된 값은 1개만 출력
+
+-- GROUP BY
+SELECT mem_id, SUM(amount) FROM buy GROUP BY mem_id;
+SELECT mem_id "회원 아이디", SUM(price*amount) "총 구매 금액" FROM buy GROUP BY mem_id;
+SELECT AVG(amount) "평균 구매 개수" FROM buy;
+SELECT mem_id "회원 아이디", AVG(amount) "평균 구매 개수" FROM buy GROUP BY mem_id;
+SELECT COUNT(*) FROM member;
+SELECT COUNT(phone1) "연락처가 있는 회원" FROM member;
+SELECT mem_id "회원 아이디", SUM(price*amount) "총 구매 금액" FROM buy WHERE SUM(price*amount)>1000 GROUP BY mem_id;
+SELECT mem_id "회원 아이디", SUM(price*amount) "총 구매 금액" 
+	FROM buy 
+	GROUP BY mem_id 
+	HAVING SUM(price*amount)>1000;
