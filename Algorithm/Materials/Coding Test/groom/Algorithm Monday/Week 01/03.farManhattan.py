d2c110413f4b937adfc9d07380f455563dd703b9
DaeYeob Kim <kimdyk2@hanmail.net>
2022-10-03 16:09:14 +0900

    [알고리즘먼데이][1회][3번] 최장 맨해튼 거리

--- Algorithm/Materials/Coding Test/groom/Algorithm Monday/Week 01/03.farManhattan.py
+++ Algorithm/Materials/Coding Test/groom/Algorithm Monday/Week 01/03.farManhattan.py
@@ -1,12 +1,10 @@
 coordinates = list(map(int,input().split()))
-result1 = abs(coordinates[2] - coordinates[0]) + abs(coordinates[3] - coordinates[1])
-result2 = 0
-for i in range(len(coordinates)):
-    for j in range(i+1,len(coordinates)):
-        result2 += abs(coordinates[i] - coordinates[j])
-        result2 += abs(coordinates[i+1] - coordinates[j+1])
-        if result2 > result1:
-            result1 = result2
+result = abs(coordinates[0] - coordinates[1]) + abs(coordinates[2] - coordinates[3])
 
-print(result1)
-print(result2)
+if abs(coordinates[0] - coordinates[2]) + abs(coordinates[1] - coordinates[3]) > result:
+    result = abs(coordinates[0] - coordinates[2]) + abs(coordinates[1] - coordinates[3])
+
+if abs(coordinates[0] - coordinates[3]) + abs(coordinates[1] - coordinates[2]) > result:
+    result = abs(coordinates[0] - coordinates[3]) + abs(coordinates[1] - coordinates[2])
+
+print(result)
